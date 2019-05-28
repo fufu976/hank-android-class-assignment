9089d3634c3e9e92e57b72cf234b68f4fb4bf0f1
fufu976 <fufu976@gmail.com>
2019-05-28 17:40:45 +0800

    作業 2 - Currency專案，計算美金並顯示

--- app/src/main/java/com/mayaw/currency/MainActivity.java
+++ app/src/main/java/com/mayaw/currency/MainActivity.java
@@ -1,13 +1,58 @@
 package com.mayaw.currency;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edNtd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        edNtd = findViewById(R.id.ed_ntd);
+    }
+
+    public void calCurrency(View v){
+        final String ntd_s = edNtd.getText().toString();
+        if(TextUtils.isEmpty(ntd_s)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Problem")
+                    .setMessage("Please enter your NTD amount")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            edNtd.setText("");
+                            edNtd.setHint("0");
+                        }
+                    })
+                    .show();
+        } else {
+            float ntd_f = Float.parseFloat(ntd_s);
+            float us_f = (float) (ntd_f / 30.9);
+            new AlertDialog.Builder(this)
+                    .setTitle("Result")
+                    .setMessage("USD is " + us_f)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            edNtd.setText("");
+                            edNtd.setHint("0");
+                        }
+                    })
+                    .show();
+        }
     }
 }
